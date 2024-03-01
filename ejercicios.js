8ce4d33ade05364de33c2e41cce7824739544e35
edwinrojas203 <161768088+edwinrojas203@users.noreply.github.com>
2024-03-01 09:43:49 -0500

    mi primer commit

---
+++ ejercicios.js
@@ -0,0 +1,19 @@
+var nombre = "santiago";
+var apellido= "rojas";
+var edad = 20;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+var numero1 = 5;
+var numero2 = 3;
+var suma = numero1 + numero2;
+console.log(suma);
+
+var tengonovia=false;
+console.log(tengonovia);
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(tengonovia));
