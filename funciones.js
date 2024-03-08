b33ce67baaa5ad5407f29b76838a3e965ea96235
edwinrojas203 <161768088+edwinrojas203@users.noreply.github.com>
2024-03-08 11:43:20 -0500

    ejercicios confuciones

---
+++ funciones.js
@@ -0,0 +1,46 @@
+function sumar (num1,num2) {
+     return num1 + num2;
+}
+
+console.log( sumar(4,30) );
+
+// Crear una funcion llamada saludar que recibe por 
+// parámetro la variable nombre 
+// Llamar la funcion con su nombre por parámetro 
+
+function saludar(nombre) {
+    return "Hola " + nombre + " como estas";
+}
+
+console.log( saludar("Felipe") );
+console.log( saludar("Duvan") );
+console.log( saludar("Eduard") );
+console.log( saludar("Daniel") );
+
+//calcular el area de un triangulo usando una funcion
+
+function calculararea(base,altura){
+return(base*altura) / 2;
+}
+
+console.log(calculararea(10,5))
+
+// crear una funcion que me calcule el IVA (19%) de un producto
+function calcularIVA(precioProducto){
+    return ( 19 * precioProducto) / 100
+}
+
+let resultado = calcularIVA(500000)
+console.log(resultado)
+
+let dia =5
+function esFinDeSimena (dia) {
+
+
+}
+
+si el dia es igual  a 6 o dia es igual a 7 entonces
+ "es fin de semana"
+
+Sino  
+  "No es fin de semana"
